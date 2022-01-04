2c5ff4ca9b2c80caa2d3af8270237b4904087228
Luisa Pinillos <luisapinillos20@gmail.com>
2022-01-03 22:43:54 -0500

    Create a file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
-# Create a file if it doesn't exist, and enter a character string.
-file {'/tmp/holberton':
-ensure  => present,
-mode    => '0744',
-owner   => 'www-data',
-group   => 'www-data',
-content => 'I love Puppet',
+# creates a file in the /tmp directory
+file { '/tmp/holberton':
+  ensure  => 'file',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
 }
