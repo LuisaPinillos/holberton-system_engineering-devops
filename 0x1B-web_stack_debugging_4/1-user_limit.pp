1429058bfbcb217f742cf7004c6010bb4cefa202
LuisaPinillos <luisapinillos20@gmail.com>
2022-05-06 10:25:45 -0500

    Fix puppet-lint

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,10 +1,11 @@
 #Change the OS configuration so that it is possible to login with
 #the holberton user and open a file without any error message.
 exec { 'Change hard':
-  command  => 'sudo sed -i 's/nofile 5/nofile 4096' /etc/security/limits.conf',
-  provider => shell,
+  command => "sed -i 's/nofile 5/ nofile 4096/' /etc/security/limits.conf",
+  path    => '/bin'
 }
+
 exec { 'Change soft':
-  command  => 'sudo sed -i 's/nofile 4/nofile 2000' /etc/security/limits.conf',
-  provider => shell,
+  command => "sed -i 's/nofile 4/ nofile 2000/' /etc/security/limits.conf",
+  path    => '/bin'
 }
