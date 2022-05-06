b882dbc27a208d3e6f40a569b7d66d2b159c5810
LuisaPinillos <luisapinillos20@gmail.com>
2022-05-06 10:12:15 -0500

    Change configuration

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,10 +1,10 @@
 #Change the OS configuration so that it is possible to login with
 #the holberton user and open a file without any error message.
 exec { 'Change hard':
-  command  => 'sudo sed -i \'s/core 100000/nofile 4096/\' /etc/security/limits.conf',
+  command  => 'sudo sed -i \'s/holberton hard nofile 5/holberton hard nofile 4096/\' /etc/security/limits.conf',
   provider => shell,
 }
 exec { 'Change soft':
-  command  => 'sudo sed -i \'s/core 0/nofile 4096\' /etc/security/limits.conf',
+  command  => 'sudo sed -i \'s/holberton soft nofile 4/holberton soft nofile 4096\' /etc/security/limits.conf',
   provider => shell,
 }
