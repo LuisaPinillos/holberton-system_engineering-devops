415e8ede1254fed0045eecfdb629d54127095bb8
LuisaPinillos <luisapinillos20@gmail.com>
2022-05-06 10:05:24 -0500

    User limit.

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,10 @@
+#Change the OS configuration so that it is possible to login with
+#the holberton user and open a file without any error message.
+exec { 'Change hard':
+  command  => 'sudo sed -i \'s/rss 10000/nofile 4096/\' /etc/security/limits.conf',
+  provider => shell,
+}
+exec { 'Change soft':
+  command  => 'sudo sed -i \'s/core 0/nofile 4096\' /etc/security/limits.conf',
+  provider => shell,
+}
